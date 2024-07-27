d7afb33cca60fd3be7d38b3b7df7ed7203ad8f17
Obliviate1 <93752735+Obliviate1@users.noreply.github.com>
2024-07-27 18:47:57 +0800

    Delete bilibili.js

--- bilibili.js
+++
@@ -1,2 +0,0 @@
-[rewrite_local]
-^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn)).*player\.(v3|v2|v1).Play(URL|View).*$ url script-request-header https://raw.githubusercontent.com/Obliviate1/OUT/main/bbbb?token=GHSAT0AAAAAACAGQD4NDDHZTCNQ27QNQCECZAVOABA
